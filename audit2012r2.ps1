2e2555935e42e41420983b21e6df2ebc52b099ac
danataka <68233447+danataka@users.noreply.github.com>
2022-01-20 07:38:08 -0800

    Modified 2019 list and ps scripts to run

---
+++ audit2012r2.ps1
@@ -0,0 +1,3 @@
+cd c:\temp\windows_hardening
+Import-Module .\Invoke-HardeningKitty.ps1
+Invoke-HardeningKitty -FileFindingList .\lists\finding_list_cis_microsoft_windows_server_2012_r2_2.4.0_machine.csv
